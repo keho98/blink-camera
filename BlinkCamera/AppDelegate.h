37ab72c7e03d242a5d819a522a8b5744ece141f4
Kevin Ho <keho98@gmail.com>
2015-04-24 20:57:57 -0700

    Initial Commit

---
+++ BlinkCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlinkCamera
+//
+//  Created by Kevin Ho on 4/24/15.
+//  Copyright (c) 2015 Keiho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlinkCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlinkCamera
+//
+//  Created by Kevin Ho on 4/24/15.
+//  Copyright (c) 2015 Keiho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
