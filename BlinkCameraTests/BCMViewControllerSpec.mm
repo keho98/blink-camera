445c759d0590258b96807262da8317c94f6bb543
Kevin Ho <keho98@gmail.com>
2015-05-20 00:09:36 -0700

    Add backfill tests

--- BlinkCameraTests/BCMViewControllerSpec.mm
+++ BlinkCameraTests/BCMViewControllerSpec.mm
@@ -1,5 +1,6 @@
 #import <Cedar/Cedar.h>
 #import "BCMViewController.h"
+#import "BCMBlinkDetector.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -8,19 +9,29 @@ SPEC_BEGIN(BCMViewControllerSpec)
 
 describe(@"BCMViewController", ^{
     __block BCMViewController *subject;
+    __block BCMBlinkDetector *blinkDetector;
 
     beforeEach(^{
         subject = [[BCMViewController alloc] init];
+        blinkDetector = nice_fake_for([BCMBlinkDetector class]);
+        subject.blinkDetector = blinkDetector;
+
+        spy_on(subject);
     });
 
     describe(@"on load", ^{
-
+        beforeEach(^{
+            subject.view should_not be_nil;
+        });
+        
+        it(@"should configure a camera session", ^{
+            subject.blinkDetector should have_received(@selector(configureNewSession));
+        });
     });
 
     describe(@"when a blink is received", ^{
         beforeEach(^{
-            spy_on(subject);
-            [subject blinkDetector:nil didReceiveBlink:nil];
+            [subject blinkDetector:blinkDetector didReceiveBlink:nil];
         });
         
         it(@"should take a photo", ^{
